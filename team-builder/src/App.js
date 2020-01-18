5bc252e6f416f956ae897ed20635fa3c5646303d
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-01-17 23:23:09 -0500

    Updated MemberItem component

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -17,6 +17,7 @@ function App() {
 
   return (
     <div className="App">
+      <MemberList members={teamMembers} />
     </div>
   );
 }
--- team-builder/src/components/MemberItem.js
+++ team-builder/src/components/MemberItem.js
@@ -5,10 +5,26 @@ const ListItem = styled.li`
 
 `;
 
-const MemberItem = props => {
-    return(
-        <ListItem>
+const P = styled.p``;
 
+const EditButton = styled.button``; 
+
+const DeleteButton = styled.button``; 
+
+
+const MemberItem = ({member, handleClick}) => {
+    const { name, email, role, id } = member;
+    return(
+        <ListItem id={id} onClick={handleClick}>
+            <P>Name: {name}</P>
+            <P>Email: {email}</P>
+            <P>Role: {role}</P>
+            <EditButton>
+                <i className="typcn-edit"></i>
+            </EditButton>
+            <DeleteButton>
+                <i className="typcn-trash"></i>
+            </DeleteButton>
         </ListItem>
     );
 }
