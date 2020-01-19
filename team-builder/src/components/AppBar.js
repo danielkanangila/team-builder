f03d26597a4dd9f88b9f714ea8841e8dcde590c1
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-01-19 00:00:12 -0500

    Adding style to app-bar

--- team-builder/src/components/AppBar.js
+++ team-builder/src/components/AppBar.js
@@ -1,38 +1,63 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Container } from './Utils';
 
 const Wrapper = styled.div`
-    display: flex;
-    height: 50px;
     width: 100%;
+    padding: 15px 0;
     border-bottom: 1px solid #ccc;
+    margin-bottom: 15px;
 `
 
 const Logo  = styled.h1`
+    margin: 0;
     font-family: 'Fredoka One', cursive;
+    font-size: 1.8rem;
 `
 
-const MenuIcon = styled.div`
-
+const MenuItemIcon = styled.div`
+    display: flex;
+    flex-direction: column;
+    cursor: pointer;
 `
 
-const UserDiv = styled.div`
-
+const MenuText = styled.span`
+    font-size: 0.55rem;
+    text-transform: uppercase;
 `
+
+const styles = {
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    icon: {
+        fontSize: '1.7rem'
+    }
+}
 
 const AppBar = () => {
+    const classes = styles;
     return(
         <Wrapper>
-            <MenuIcon>
-                <i className="fas fa-bars"></i>
-                Menu
-            </MenuIcon>
-            <Logo>
-                AdminPanel 
-            </Logo>
-            <UserDiv>
-                <i className="fas fa-user-circle"></i>
-            </UserDiv>
+            <Container style={classes.container}>
+                <MenuItemIcon>
+                    <i style={classes.icon} className="fas fa-bars"></i>
+                    <MenuText>
+                        Menu
+                    </MenuText>
+                </MenuItemIcon>
+                <Logo>
+                    AdminPanel 
+                </Logo>
+                <MenuItemIcon>
+                    <i className="fas fa-user-circle"></i>
+                    <MenuText>
+                        Me
+                    </MenuText>
+                </MenuItemIcon>
+            </Container>
         </Wrapper>
     )
 }
